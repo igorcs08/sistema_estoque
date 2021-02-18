08494c3a714660305a2c2d9da3ecb3aae83e125a
Igor Cavalcante <igor.cavalcante96@gmail.com>
2021-02-17 21:06:01 -0300

    terceiro commit

--- igor/Produtos.py
+++ igor/Produtos.py
@@ -7,15 +7,36 @@ class Produtos:
     def cadastrarProduto(self, nome, marca, preco_C, preco_V, qtd):        
         self.lista.append(Produto(nome, marca, preco_C, preco_V, qtd))
     
+    def exibirProduto(self, item):        
+        return item.nome + " " + item.marca + " " + str(item.preco_C) + " " + str(item.preco_V) + " " + str(item.qtd) + "\n"
+
     def visualizarProdutos(self, item):
-        dados = ""
-        for i in item:
-            dados += i.nome + " " + i.marca + " " + str(i.preco_C) + " " + str(i.preco_V) + " " + str(i.qtd) + "\n"
-        return dados
+        if self.lista != []:
+            dados = ""
+            for i in item:
+                dados += self.exibirProduto(i)
+            return dados
+        else:
+            return "Nao ha itens a serem exibidos!\n"
     
-    def excluirProduto(self, item):
-        return None
+    def buscarItem(self, nome):        
+        for item in self.lista:
+            if (item.nome == nome):
+                return self.lista.index(item)
+                
+    def excluirProduto(self, nome):
+        indice = self.buscarItem(nome)
+        
+        if indice != None:
+            removido = self.lista.pop(indice)
+            return "O item " + removido.nome +" foi removido com sucesso!\n"
+        else:
+            return "Item nao existe na lista!\n"
     
-    def alterarProduto(self):
+    def alterarProduto(self, nome):
+        indice = self.buscarItem(nome)
+        if (indice != None):
+            print(self.exibirProduto(self.lista[indice]))            
+
         return None
 
--- igor/main.py
+++ igor/main.py
@@ -2,5 +2,11 @@ from produtos import Produtos
 teste = Produtos()
 
 teste.cadastrarProduto('sabao', 'omo', 9.3, 10.5, 50)
+teste.cadastrarProduto('arroz', 'tio_joao', 3.3, 4.5, 100)
+teste.cadastrarProduto('feijao', 'gostozzo', 6.4, 9.5, 30)
+teste.cadastrarProduto('farinha_de_trigo', 'dona_benta', 5.5, 6.8, 25)
 
+
+print(teste.visualizarProdutos(teste.lista))
+print(teste.excluirProduto('arroz'))
 print(teste.visualizarProdutos(teste.lista))
