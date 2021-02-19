8ac0bd53dde22a2535a955443060e7ae64f9c3a9
Igor Cavalcante <igor.cavalcante96@gmail.com>
2021-02-19 01:51:33 -0300

    sexto commit

--- igor/Produtos.py
+++ igor/Produtos.py
@@ -40,52 +40,40 @@ class Produtos:
         else:
             return "Item nao existe na lista!\n"
     
-    def alterarProduto(self, id):
+    def alterarProduto(self, id, opcao, dado):
+        
         indice = self.buscarItem(id)
-        if (indice != None):
-            print(self.exibirProduto(self.lista[indice]))
-            print("O que voce deseja alterar?\n1-Nome\n2-Marca\n3-Preco de custo\n4-Preco de venda\n5-Quantidade\n6-Sair\n")
-            n = int(input())
-            if(n == 1):
-                print("Digite o nome:")
-                self.lista[indice].setNome(input())
-                pass
-            if(n == 2):
-                print("Digite a marca:")
-                self.lista[indice].setMarca(input())
-                pass
-            if(n == 3):
-                print("Digite o preco de custo:")
-                self.lista[indice].setPreco_C(input())
-                pass
-            if(n == 4):
-                print("Digite o preco de venda:")
-                self.lista[indice].setPreco_V(input())
-                pass
-            if(n == 5):
-                print("Digite a quantidade:")
-                self.lista[indice].setQtd(input())
-                pass
-            return "Item alterado com sucesso!\n"
-        else:
-            return "Item buscado para alteracao nao existe!\n"
+        
+        if(opcao == 1):                
+            self.lista[indice].setNome(dado)
+            pass
+        if(opcao == 2):                
+            self.lista[indice].setMarca(dado)
+            pass
+        if(opcao == 3):                
+            self.lista[indice].setPreco_C(float(dado))
+            pass
+        if(opcao == 4):                
+            self.lista[indice].setPreco_V(float(dado))
+            pass
+        if(opcao == 5):                
+            self.lista[indice].setQtd(int(dado))
+            pass
+        return "Item alterado com sucesso!\n"
+        
         
     
-    def transacaoSaida(self):
-        print(self.visualizarProdutos(self.lista))
-        print("Qual item desejas fazer retirada?\nObs.:Buscar pelo ID")
-        item = int(input())
+    def transacaoSaida(self, item, qntd):        
         indice = self.buscarItem(item)
-        print("Quantidade que desejas fazer retirada?")
-        qntd = int(input())
         qntd_atual = self.lista[indice].getQtd()
-        if (qntd_atual >= qntd > 0):
-            self.lista[indice].setQtd(qntd_atual-qntd)
-            transacao = str(self.lista[indice].getId())+" "+self.lista[indice].getNome()+" "+ self.lista[indice].getMarca() +" "+ str(qntd) +" "+self.data_atual()+"\n"
-            self.historico_saida.append(transacao)
-            print("Quantidade retirada com sucesso!")
-        else:
-            print("Nao houve retirada!")
+        if (indice != None):
+            if (qntd_atual >= qntd > 0):
+                self.lista[indice].setQtd(qntd_atual-qntd)
+                transacao = str(self.lista[indice].getId())+" "+self.lista[indice].getNome()+" "+ self.lista[indice].getMarca() +" "+ str(qntd) +" "+self.data_atual()+"\n"
+                self.historico_saida.append(transacao)
+                return "Quantidade retirada com sucesso!"
+            else:
+                return "Nao houve retirada!"
         
     def data_atual(self):
         data_hora = datetime.now()
--- igor/main.py
+++ igor/main.py
@@ -7,11 +7,108 @@ teste.cadastrarProduto('feijao', 'gostozzo', 6.4, 9.5, 30)
 teste.cadastrarProduto('farinha_de_trigo', 'dona_benta', 5.5, 6.8, 25)
 
 
-print(teste.visualizarProdutos(teste.lista))
-print(teste.excluirProduto(2))
-print(teste.visualizarProdutos(teste.lista))
-print(teste.alterarProduto(3))
-print(teste.visualizarProdutos(teste.lista))
-teste.transacaoSaida()
-print(teste.visualizarProdutos(teste.lista))
-print(teste.historico_saida)
+while True:
+    print("1 - MANTER PRODUTOS\n2 - REALIZAR TRANSACAO DE ENTRADA\n3 - REALIZAR TRANSACAO DE SAIDA\n4 - EXIBIR HISTORICO DE ENTRADAS\n5 - EXIBIR HISTORICO DE SAIDAS\n6 - SAIR\n")
+    n = int(input())
+    if(n == 1):
+        #MANTER PRODUTOS
+        while True:
+            print("1 - CADASTRAR PRODUTO\n2 - EXIBIR PRODUTOS\n3 - ALTERAR PRODUTO\n4 - EXCLUIR PRODUTO\n5 - SAIR\n")
+            m = int(input())
+            if(m == 1):
+                #CADASTRAR PRODUTOS
+                print("Digite o nome do produto:")
+                nome = input()
+                print("Digite a marca do produto:")
+                marca = input()
+                print("Digite o preco de custo:")
+                preco_c = float(input())
+                print("Digite o preco de venda:")
+                preco_v = float(input())
+                print("Digite a quantidade de produtos:")
+                qntd = int(input())
+                teste.cadastrarProduto(nome, marca, preco_c, preco_v, qntd)
+                pass
+            if(m == 2):
+                #EXIBIR PRODUTOS
+                print(teste.visualizarProdutos(teste.lista))
+                pass
+            if(m == 3):
+                #ALTERAR PRODUTOS
+                print(teste.visualizarProdutos(teste.lista))
+                print("Digite o ID do item que desejas alterar:")
+                id = int(input())
+                indice = teste.buscarItem(id)
+                if (indice != None):
+                    print(teste.exibirProduto(teste.lista[indice]))
+                    print("O que voce deseja alterar?\n1-Nome\n2-Marca\n3-Preco de custo\n4-Preco de venda\n5-Quantidade\n6-Sair\n")
+                    n = int(input())
+                    if(n == 1):
+                        print("Digite o nome:")
+                        dado = input()
+                        print(teste.alterarProduto(id, n, dado))
+                        pass
+                    if(n == 2):
+                        print("Digite a marca:")
+                        dado = input()
+                        print(teste.alterarProduto(id, n, dado))
+                        pass
+                    if(n == 3):
+                        print("Digite o preco de custo:")
+                        dado = float(input())
+                        print(teste.alterarProduto(id, n, dado))
+                        pass
+                    if(n == 4):
+                        print("Digite o preco de venda:")
+                        dado = float(input())
+                        print(teste.alterarProduto(id, n, dado))
+                        pass
+                    if(n == 5):
+                        print("Digite a quantidade:")
+                        dado = int(input())
+                        print(teste.alterarProduto(id, n, dado))
+                        pass
+                else:
+                    print("Item buscado para alteracao nao existe!\n")
+                pass
+
+            if(m == 4):
+                #EXCLUIR PRODUTOS
+                print(teste.visualizarProdutos(teste.lista))
+                print(teste.excluirProduto(2))
+                pass
+            if(m == 5):
+                break
+        pass
+    if(n == 2):
+        #REALIZAR TRANSACAO DE ENTRADA
+
+        pass
+    if(n == 3):
+        #REALIZAR TRANSACAO DE SAIDA
+        print(teste.visualizarProdutos(teste.lista))
+        print("Qual item desejas fazer retirada?\nObs.:Buscar pelo ID")
+        item = int(input())        
+        print("Quantidade que desejas fazer retirada?")
+        qntd = int(input())        
+        
+        print(teste.transacaoSaida(item, qntd))
+
+        pass
+    if(n == 4):
+        #EXIBIR HISTORICO DE ENTRADAS
+
+        pass
+    if(n == 5):
+        #EXIBIR HISTORICO DE SAIDAS
+        print(teste.historico_saida)
+        pass
+    if(n == 6):        
+        break
+
+
+
+
+
+
+
